dade311a0fa8249f9af87188d72e3d024a0d3669
gecko655 <aqwsedrft1234@yahoo.co.jp>
2016-06-20 23:27:26 +0900

    Use minute for unit

--- src/rec.sh
+++ src/rec.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-  echo "usage: $0 length-to-record[sec] [title]"
+  echo "usage: $0 length-to-record[min] [title]"
   exit 1
 fi
-length=$1
+length=$(($1 * 60))
 title=`date "+%Y%m%d"`-${2-agqr}
 
 /usr/local/bin/rtmpdump -r rtmp://fms-base1.mitene.ad.jp/agqr/aandg22 --live -B $length -o /tmp/$title.flv
