c8135882dc783c8d676f4c0f5fdd5c7590618396
JUNG <sangwon.jung.work@gmail.com>
2020-11-15 21:43:00 +0900

    for agqr hls service

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM ubuntu:16.04
+FROM ubuntu:18.04
 MAINTAINER swjung89 <sangwon-jung-work@gmail.com>
 
-RUN apt-get update -y && apt-get install -y software-properties-common build-essential libxml2-utils rtmpdump wget git zlib1g-dev tzdata
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update -y && apt-get install -y software-properties-common build-essential libxml2-utils wget curl git libxml2-utils openssl libssl-dev tzdata
 RUN ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 RUN mkdir /var/src
 WORKDIR /var/src
+
 RUN wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz && tar zxvf yasm-1.2.0.tar.gz && cd yasm-1.2.0 && ./configure && make && make install
-RUN git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg && cd ffmpeg && ./configure && make && make install
-RUN git clone git://github.com/matthiaskramm/swftools.git swftools && cd swftools && ./configure && make && make install
+RUN git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg && cd ffmpeg && ./configure --enable-openssl && make && make install
+
 RUN echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf && ldconfig
 ADD rec_agqr.sh /usr/local/bin/rec_agqr.sh
 RUN chmod +x /usr/local/bin/rec_agqr.sh
--- rec_agqr.sh
+++ rec_agqr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# 2020.03.22 URL aandg1b -> aandg1
+# 2020.11.13 copy rec_agqr.sh
+#
 
 if [ $# -lt 1 ]; then
   echo "usage: $0 length-to-record[min] [title] [outpath]"
@@ -12,24 +13,100 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
+
 length=$(($1 * 60))
 DATES=`date '+%Y%m%d'`
 title=${2-agqr}_$DATES
 outdir="."
+
+BASE_URL=https://www.uniqueradio.jp/agplayer5
+
+
+# check essential tools
+if ! curl --version &> /dev/null
+then
+    echo "curl could not be found"
+    exit 1
+fi
+
+if ! xmllint --version &> /dev/null
+then
+    echo "xmllint could not be found"
+    exit 1
+fi
+
+if ! ffmpeg -version &> /dev/null
+then
+    echo "ffmpeg could not be found"
+    exit 1
+fi
+
+
 
 if [ $# -ge 3 ]; then
   outdir=$3
 fi
 
 
-/usr/bin/rtmpdump -r rtmp://fms-base1.mitene.ad.jp/agqr/aandg1 --live -B $length -o /tmp/$title.flv
 
+# get player php url in main page's html
+curl --insecure "${BASE_URL}/player.php" -X GET | grep php > "${outdir}/tempTag1_${title}"
 
-# convert mp4 video
-ffmpeg -loglevel quiet -y -i "/tmp/${title}.flv" -vcodec copy -acodec copy "${outdir}/${title}.mp4"
-
-
-if [ $? = 0 ]; then
-    rm -f "/tmp/${title}.flv"
-    # mv /tmp/$title.flv $outdir 
+if [ $? != 0 ]; then
+  echo "error player.php" > "${outdir}/tempTag1Error_${title}"
 fi
+
+#echo "player"
+#cat "${outdir}/tempTag2_${title}"
+
+playerurl=$( xmllint --html --xpath //iframe/@src "${outdir}/tempTag1_${title}" | sed 's/ src="\([^"]*\)"/\1/g' )
+
+#echo "playerurl"
+#echo $playerurl
+
+
+
+# get m3u8 line in player php page's html
+curl --insecure "${BASE_URL}/${playerurl}" -X GET | grep m3u8 > "${outdir}/tempTag2_${title}"
+
+if [ $? != 0 ]; then
+  echo "error iframe.php" > "${outdir}/tempTag2Error_${title}"
+fi
+
+#echo "source"
+#cat "${outdir}/tempTag2_${title}"
+
+# parse src string
+listurl=$( xmllint --html --xpath //source/@src "${outdir}/tempTag2_${title}" | sed 's/ src="\([^"]*\)"/\1/g' )
+
+#echo "listurl"
+#echo $listurl
+
+
+
+# get recording streaming url
+curl $BASE_URL/$listurl > "${outdir}/tempUrl_${title}"
+
+if [ $? != 0 ]; then
+  echo "error url" > "${outdir}/tempUrl_Error_${title}"
+fi
+
+streamurl=$( grep -m 1 -Rh m3u8 "${outdir}/tempUrl_${title}" )
+
+#echo "streamurl"
+#echo $streamurl
+
+
+
+# start record
+ffmpeg -i "${streamurl}" -vcodec copy -acodec copy -t $length "${outdir}/${title}.mp4"
+
+
+
+# remove temp files
+if [ $? = 0 ]; then
+  rm -f "${outdir}/tempTag1_${title}" "${outdir}/tempTag2_${title}" "${outdir}/tempUrl_${title}"
+else
+  rm -f "${outdir}/tempTag1_${title}" "${outdir}/tempTag2_${title}" "${outdir}/tempUrl_${title}"
+fi
+
