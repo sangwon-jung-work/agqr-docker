18fb19defaa095fb07c6b481d9b71ca5b10eaff3
SangWon Jung <sangwon.jung.work@gmail.com>
2023-06-29 00:16:44 +0900

    change buffer time and break count, add sleep interval before fail retry

--- rec_agqr_failover.sh
+++ rec_agqr_failover.sh
@@ -2,6 +2,8 @@
 # agqr new recording shell(with while loop)
 # 2022.03.04 first create
 # 2022.04.02 change while condition le to lt
+# 2023.06.28 change buffer time 35 to 30 seconds, change break count 15 to 10
+# 2023.06.29 add sleep interval before fail retry
 #
 # ./rec_agqr_failover.sh 30 "AGQR_REC"
 #
@@ -19,28 +21,30 @@ PREFIX=$2
 # change for each Env
 REC_HOME=/workspace
 DOCKER_HOME=/usr/bin/docker
+SLEEP_HOME=/usr/bin/sleep
 IMAGE_NAME="(image_name):(image_version)"
 
 # for failover
 TOTAL_SECONDS=$(($REC_MIN * 60))
-BUFF_SECONDS=$(($TOTAL_SECONDS - 35))
+BUFF_SECONDS=$(($TOTAL_SECONDS - 30))
+RETRY_INTERVAL_SECONDS=15
 FAIL_COUNT=
 
 echo "${PREFIX} agqr recoder will running ${TOTAL_SECONDS} sec"
 
 $DOCKER_HOME run --rm -v $REC_HOME:/var/agqr $IMAGE_NAME $REC_MIN $PREFIX .
 
-# Rework if finished 35 seconds earlier than the original work time
+# Rework if finished 30 seconds earlier than the original work time
 while [ "$SECONDS" -lt "$BUFF_SECONDS" ]
 do
   echo "${PREFIX} agqr recoder finished less than ${BUFF_SECONDS} sec."
   
   FAIL_COUNT=$[ $FAIL_COUNT +1]
   
-  # if FAIL_COUNT is grater than 15, break while
-  if [ "$FAIL_COUNT" -gt 15 ];
+  # if FAIL_COUNT is grater than 10, break while
+  if [ "$FAIL_COUNT" -gt 10 ];
   then
-    echo "FAIL_COUNT ${FAIL_COUNT} is grater than 15. stop recording"
+    echo "FAIL_COUNT ${FAIL_COUNT} is grater than 10. stop recording"
     break
   fi
   
@@ -55,7 +59,7 @@ do
   REC_MIN_RECAL=$(($REC_MIN - ((hrs*60) + $mins)))
   TOTAL_SECONDS=$(($REC_MIN_RECAL * 60))
   
-  echo "${PREFIX} agqr recoder will running ${TOTAL_SECONDS} sec"
+  echo "${PREFIX} agqr recoder will running ${TOTAL_SECONDS} sec after ${RETRY_INTERVAL_SECONDS} later"
   
   # if REC_MIN_RECAL is negative number, break while
   if [ "$REC_MIN_RECAL" -lt 0 ];
@@ -63,6 +67,8 @@ do
     echo "REC_MIN_RECAL ${REC_MIN_RECAL} is cannot negative value. stop recording"
     break
   fi
+  
+  $SLEEP_HOME $RETRY_INTERVAL_SECONDS
   
   $DOCKER_HOME run --rm -v $REC_HOME:/var/agqr $IMAGE_NAME $REC_MIN_RECAL $PREFIX$FAIL_COUNT .
   
